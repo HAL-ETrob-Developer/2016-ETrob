60033e5f9c260f9968c4acae8bf9bdf5be40f86f
ohs50465 <ohs50465>
2016-07-14 19:14:13 +0900

    update

--- Source/mainapp/LineTracer_ohs.cpp
+++ Source/mainapp/LineTracer_ohs.cpp
@@ -3,8 +3,10 @@
 /**
  * コンストラクタ
  */
-LineTracer_ohs::LineTracer_ohs()
-    : m() {
+LineTracer_ohs::LineTracer_ohs( RayReflectAdomin_ohs* ray_reflect_adomin )
+{
+    mRayReflectAdomin = ray_reflect_adomin;
+
 }
 
 /**
@@ -16,23 +18,53 @@ LineTracer_ohs::~LineTracer_ohs() {
 /**
  * ライントレース指揮
  */
-void LineTracer_ohs::execLineTraceConduct() {
+void LineTracer_ohs::postLineTraceConduct() {
+    public:
+    /* 光学反射値の取得 */
+    numColor = mRayReflectAdomin -> getState();
+    /* ラインが白か否かの判別 */
+    if(numColor == COLOR_WHITE) {
+    /* 白ならラインエッジトレースへ */
+    execLineEdgeTrace();
+    }
+    else {
+    /* 白以外ならライン探索へ */
+    execLineSearch();
+    }
+    private:
 }
 
 /**
  * ライントレース実行
  */
 void LineTracer_ohs::callLineTraceAct() {
+
 }
 
 /**
  * ラインエッジトレース
  */
 void LineTracer_ohs::execLineEdgeTrace() {
+    /* ライン探索ゲインのリセット */
+
+    /* 光学センサ値の取得 */
+    
+    /* 左右モータの実指示値を取得 */
+
+    /* PID計算 */
+
+    /* 走行速度・角度の決定 */
 }
 
 /**
  * ラインサーチ
  */
 void LineTracer_ohs::execLineSearch() {
+    /* ライン探索ゲインの取得 */
+
+    /* ゲイン比に基づき走行速度減 */
+
+    /* ゲイン比に基づき走行角度をCCWへ */
+
+    /* ライン探索ゲインの上昇 */    
 }
