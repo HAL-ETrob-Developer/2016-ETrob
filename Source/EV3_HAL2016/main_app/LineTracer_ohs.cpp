835d8b92e1508cfbe10f0fc312cc6060693c8895
45025 ohs <45025 ohs>
2016-07-15 06:43:01 +0900

    #10 中間レビュー

--- Source/EV3_HAL2016/main_app/LineTracer_ohs.cpp
+++ Source/EV3_HAL2016/main_app/LineTracer_ohs.cpp
@@ -1,10 +1,22 @@
+#include "ev3api.h"
+#include "hal_ev3_std.h"
+#include "RunningAdmin_ohs.h"
+#include "RayReflectAdmin_ohs.h"
+#include "RunLineCalculator_ohs.h"
+
 #include "LineTracer_ohs.h"
 
 /**
  * コンストラクタ
  */
-LineTracer_ohs::LineTracer_ohs()
-    : m() {
+LineTracer_ohs::LineTracer_ohs( RunningAdmin_ohs* running_admin,,RayReflectAdomin_ohs* ray_reflect_adomin, RunningLineCalculator_ohs* running_line_calculator )
+:mRunningAdmin(running_admin)
+ mRayReflectAdomin( ray_reflect_adomin ),
+ mRunningLineCalclator( running_line_calclator )
+{
+    mGetColor = SCLR_GRAY;
+    mSpeed = 0;
+    mDeg   = 0;
 }
 
 /**
@@ -17,22 +29,70 @@ LineTracer_ohs::~LineTracer_ohs() {
  * ライントレース指揮
  */
 void LineTracer_ohs::postLineTraceConduct() {
+    mLineTraceGo = true;
 }
 
 /**
  * ライントレース実行
  */
 void LineTracer_ohs::callLineTraceAct() {
+    //実行指揮の確認 
+    if( mLineTraceGo == false ) { return; }//ライントレース指揮無し
+    //フラグリセット
+    mLineTraceGo = false;
+
+    /* 光学反射値の取得 */
+    mGetColor = mRayReflectAdomin->getReflectValue();
+    /* ラインが白か否かの判別 */
+    if( mGetColor == SCLR_WHITE ) {
+        /* 白ならラインエッジトレースへ */
+        execLineEdgeTrace();
+    } else {
+        /* 白以外ならライン探索へ */
+        execLineSearch();
+    }
+
+    //走行指示
+    mRunningAdmin->postRunning( mSpeed, mDeg, true );//(走行速度,走行角度,バランス制御の有無)
+
+    return;
 }
 
 /**
  * ラインエッジトレース
  */
 void LineTracer_ohs::execLineEdgeTrace() {
+    /* ライン探索ゲインのリセット */
+
+    /* 光学センサ値の取得 */
+    //mRayReflectAdomin->getReflectState();<-mGetColorに入ってます
+    /* 左右モータの実指示値を取得(保留) */
+
+    /* PID計算 */
+    mRunningLineCalculator->calcRunningLine( mGetColor, &mSpeed, &mDeg );
+    /* 走行速度 */
+    if( mSpeed > MAX_SPEED ){
+        mSpeed = MAX_SPEED;
+    } eise if( mSpeed < MIN_SPEED ) {
+        mSpeed = MIN_SPEED;
+    }
+    /* 角度の決定 */
+    if( mDeg > MAX_DEGRE ){
+        mDeg = MAX_DEGRE;
+    } eise if( mDeg < MIN_DEGRE ) {
+        mDeg = MIN_DEGRE;
+    }
 }
 
 /**
  * ラインサーチ
  */
 void LineTracer_ohs::execLineSearch() {
+    /* ライン探索ゲインの取得 */
+
+    /* ゲイン比に基づき走行速度減 */
+
+    /* ゲイン比に基づき走行角度をCCWへ */
+
+    /* ライン探索ゲインの上昇 */    
 }
--- Source/EV3_HAL2016/main_app/LineTracer_ohs.h
+++ Source/EV3_HAL2016/main_app/LineTracer_ohs.h
@@ -3,20 +3,33 @@
 #ifndef MAINAPP_LINETRACER_OHS_H_
 #define MAINAPP_LINETRACER_OHS_H_
 
+#define MAX_SPEED (      100 )
+#define MIN_SPEED (     -100 )
+#define MAX_DEGRE (       50 )
+#define MIN_DEGRE (      -50 )
+
 class LineTracer_ohs {
 public:
     //生成
-    LineTracer_ohs();
+    LineTracer_ohs( RunningAdmin_ohs* running_admin, RayReflectAdomin_ohs* ray_reflect_adomin, RunningLineCalculator_ohs* running_line_calculator );
     //デストラクタ 死ぬときあるよ
-        virtual ~LineTracer_ohs();
+    ~LineTracer_ohs();
 
     void postLineTraceConduct();
     void callLineTraceAct();
-    void execLineEdgeTrace();
-    void execLineSearch();
 
 private:
-
+    //メソッド
+    void execLineEdgeTrace();
+    void execLineSearch();
+    //メンバ
+    RunningAdmin_ohs* mRunningAdmin;
+    RayReflectAdomin_ohs*  mRayReflectAdomin;
+    RunningLineCalculator_ohs*  mRunningLineCalclator;
+    SENC_CLR mGetColor;
+    BOOL mLineTraceGo;
+    int8_t mSpeed;
+    int8_t mDeg;
 };
 
 #endif  // MAINAPP_LINETRACER_OHS_H_
