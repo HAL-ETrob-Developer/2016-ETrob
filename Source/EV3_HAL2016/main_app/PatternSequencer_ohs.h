64b9bd343db9f335455096801e5c88a024de70d9
ohs50465 <ohs50465>
2016-08-07 20:50:29 +0900

    パターンシーケンサー更新

--- Source/EV3_HAL2016/main_app/PatternSequencer_ohs.h
+++ Source/EV3_HAL2016/main_app/PatternSequencer_ohs.h
@@ -2,8 +2,11 @@
 
 #ifndef MAINAPP_PATTERNSEQUENCER_OHS_H_
 #define MAINAPP_PATTERNSEQUENCER_OHS_H_
+
+/* include file */
 #include "hal_ev3_std.h"
 
+/* 構造体 実行動作インデックス */
 typedef struct {
   int32_t RunningSpeed;
   int32_t RunningAngle;
@@ -15,13 +18,15 @@ class PatternSequencer_ohs {
 public:
     //生成
     PatternSequencer_ohs();
-    //デストラクタ 死ぬときあるよ
+    //デストラクタ
         virtual ~PatternSequencer_ohs();
 
-    void callPatternRunning();
+    SCHR callPatternRunning( UCHR InputNumber, BOOL Balancer );
 
 private:
-
+  RUNNING_PAT_INDEX ActionIndex;
+  RunningAdmin_ohs*      mRunningAdmin;
+  TailAdmin_ohs*         mTailAdmin;
 };
 
 #endif  // MAINAPP_PATTERNSEQUENCER_OHS_H_
