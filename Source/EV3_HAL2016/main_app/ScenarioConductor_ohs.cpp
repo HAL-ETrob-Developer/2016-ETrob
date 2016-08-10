019d93acb3c34fde404017589e17693e17904a95
ToruUeno <git>
2016-08-11 00:33:58 +0900

    競技シナリオ指揮者更新

--- Source/EV3_HAL2016/main_app/ScenarioConductor_ohs.cpp
+++ Source/EV3_HAL2016/main_app/ScenarioConductor_ohs.cpp
@@ -10,11 +10,13 @@
 /**
  * コンストラクタ
  */
-ScenarioConductor_ohs::ScenarioConductor_ohs( LineTracer_ohs* line_tracer, BodyStateAdmin_ohs* body_state_admin, PatternSequencer_ohs* pattern_sequencer )
+ScenarioConductor_ohs::ScenarioConductor_ohs( LineTracer_ohs* line_tracer, BodyStateAdmin_ohs* body_state_admin, PatternSequencer_ohs* pattern_sequencer,TailAdmin_ohs* tail_admin, RunningAdmin_ohs* running_admin )
 {
 	mLineTracer 			= line_tracer;
 	mBodyStateAdmin 	= body_state_admin;
 	mPatternSequencer = pettern_sequencer;
+	mTailAdmin        = tail_admin;
+	mRunningAdmin     = running_admin;
 
 	mMilage = 0;
 	mBodyAngle = 0;
@@ -34,13 +36,40 @@ BOOL ScenarioConductor_ohs::execScenario() {
 
 	/* 実行動作インデックスの番号に応じて指揮する */
 	switch ( mIndex.action_number ) {
-		case LINE_TRACE;
-		/*　ライントレース指揮 */
+		case LINE_TRACE:        /*　ライントレース指揮 */
 		mLineTracer -> postLineTraceConduct();
 		break;
 
-		case PATTERN_RUNNING;
-		/* 定量走行指揮 */
+		case PATTERN_RUNNING:   /* 定量走行指揮 */
+		mPatternSequencer -> callPatternRunning();
+		break;
+
+		case LOOKUP_GATE:
+		/* 走行体を傾ける */
+		mTailAngle = TargetTailAngle;              /* 尾角度指定 */
+		TailAdmin -> postTailDegree( mTailAngle ); /* 尾角度指示 */
+
+		/* グレーゾーンからスタートし、ルックアップゲート通過直後の場所まで距離を進める */
+		mPatternSequencer -> callPatternRunning();
+		if( mMilage >= TargetMilage2 - /* ルックアップゲート通過直後の場所になるよう調整 */ )　{
+		/* ルックアップゲート通過前の場所まで後退する */
+		while( mMilage >= TargetMilage1 + /*ルックアップゲート手前の場所になるよう調整 */ ) {
+			/* 後退処理 */
+		}
+		/* 次のグレーを検知するまで前進する */
+		mPatternSequencer -> callPatternRunning();
+		break;
+
+		case STEP:
+		/* 1段目に上るため速度を上げる */
+
+		/* 登ったら一旦停止し、右に360度回転する */
+
+		/* 再び走行開始 2段目を上る */
+
+		/* 登ったら一旦停止し、右に360度回転する */
+
+		/* 走行速度を元に戻し、次のグレーを検知するまで前進する */
 		mPatternSequencer -> callPatternRunning();
 		break;
 	}
@@ -63,43 +92,65 @@ BOOL ScenarioConductor_ohs::execScenario() {
 	/* 尾角度取得 */
 	mTailAngle = mBodyStateAdmin -> getTailAngle();
 
-	//グレー検知したら達成条件を更新する
+  /* 達成条件更新 --------------------------------------------------- */
 	if( mColorSensorState == SCLR_GRAY ) {
-		mIndex.event_condition++;
+		//ゴール後
+		if( mMilage >= TargetMilage && mMilage < TargetMilage2 ) {
+			mIndex.event_condition = TRACE_CLEAR;
+		}
+
+		//ルックアップゲート通過後
+		else if( mMilage >= TargetMilage2 && mMilage < TargetMilage3 ) {
+			mIndex.event_condition = HURDLE1_CLEAR;
+		}
+
+
 		switch( mIndex.event_condition ) {
-			case TRACE_CLEAR:			//トレースクリア
-				/* 現本体状態の記録 ------------------------------------------- */
+
+      case NO_CLEAR:       //トレース中
+				setScenario( mRoute );
+				break;
+
+			case TRACE_CLEAR:    //トレースクリア
+			/* 現本体状態の記録 ------------------------------------------- */
 				ColorSensorState = mColorSensorState;
-				BalanceState		 = mBalanceState;
-				Milage					 = mMilage;
-				BodyAngle				 = mBodyAngle;
-				TailAngle				 = mTailAngle;
+				BalanceState     = mBalanceState;
+				Milage           = mMilage;
+				BodyAngle        = mBodyAngle;
+				TailAngle        = mTailAngle;
+
 
 				setScenarioUpDate();//シナリオ更新
 				setScenario( mRoute );
 			break;
 
 			case HURDLE1_CLEAR:		//難所1（ルックアップゲートor階段）クリア
+        TailAdmin -> postTailDegree( TailAngle ); /* 尾角度を元に戻す指示をする */
 				/* 現本体状態の記録 ------------------------------------------- */
 				ColorSensorState = mColorSensorState;
-				BalanceState		 = mBalanceState;
-				Milage					 = mMilage;
-				BodyAngle				 = mBodyAngle;
-				TailAngle				 = mTailAngle;
+				BalanceState     = mBalanceState;
+				Milage           = mMilage;
+				BodyAngle        = mBodyAngle;
+				TailAngle        = mTailAngle;
 
-				setScenarioUpDate();//シナリオ更新
-				setScenario( mRoute );
-				break;
 
-			case HURDLE2_CLEAR:		//難所2(ガレージイン)クリア
-				quitCommand();			//指揮終了
-				break;
-
-			default:							//トレース中
+				setScenarioUpDate(); //シナリオ更新
 				setScenario( mRoute );
 				break;
 		}
 	}
+
+	//ガレージインの処理
+	if( mMilage >= TargetMilage3 ) {
+		mIndex.event_condition = HURDLE2_CLEAR;
+		/* 3秒間完全停止処理 */
+	}
+
+	if(mIndex.event_condition == HURDLE2_CLEAR ) {
+			quitCommand(); //指揮終了
+			break;
+	}
+
 }
 
 /**
@@ -117,25 +168,35 @@ SCHR ScenarioConductor_ohs::setScenario( RUNNING_ROUTE route ) {
 		switch ( flow_transition ) {
 			case TRACE;
 				/* トレース */
+				TargetMilage1 = ;/* スタートからゴールまでの距離 */
 				break;
 			case HURDLE1;
 				/* ルックアップゲート */
+				mIndex.action_number = LOOKUP_GATE;
+				TargetMilage2   = ;/* スタートからルックアップゲート通過後のグレーまでの距離 */
+				TargetTailAngle = ;/* ルックアップゲートを通過できる角度 */
 				break;
 			case HURDLE2;
 				/* ガレージイン */
+				TargetMilage3 = ;/* スタートからガレージ内停止位置までの距離 */
+
 				break;
 		}
 	}
-	else if (route == RROUTE_RIGHT ) {
+	else if ( route == RROUTE_RIGHT ) {
 		switch ( flow_transition ) {
 			case TRACE;
 				/* トレース */
+				TargetMilage1 = ;/* スタートからゴールまでの距離 */
 				break;
 			case HURDLE1;
 				/* 階段 */
+				mIndex.action_number = STEP;
+				TargetMilage2 = ;/* ゴールから階段通過後のグレーまでの距離 */
 				break;
 			case HURDLE2;
 				/* ガレージイン */
+				TargetMilage3 = ;/* 階段通過後のグレーからガレージ内停止位置までの距離 */
 				break;
 		}
 	}
--- Source/EV3_HAL2016/main_app/ScenarioConductor_ohs.h
+++ Source/EV3_HAL2016/main_app/ScenarioConductor_ohs.h
@@ -8,23 +8,28 @@
 #include "BodyStateAdmin_ohs.h"
 
 /* 達成条件 */
-#define TRACE_CLEAR     (    1  )
-#define HURDLE1_CLEAR   (    2  )
-#define HURDLE2_CLEAR   (    3  )
+#define NO_CLEAR        (    0  ) //ゴールしていない
+#define TRACE_CLEAR     (    1  ) //ゴールした
+#define HURDLE1_CLEAR   (    2  ) //難所クリア
+#define HURDLE2_CLEAR   (    3  ) //ガレージインクリア
 
 /* 次の遷移番号 */
-#define TRACE           (    0  )
-#define HURDLE1         (    1  )
-#define HURDLE2         (    2  )
+#define TRACE           (    0  ) //トレース
+#define HURDLE1         (    1  ) //難所
+#define HURDLE2         (    2  ) //ガレージイン
 
 /* 実行動作番号 */
-#define LINE_TRACE      (   '1' )
-#define PETTERN_RUNNING (   '2' )
+#define LINE_TRACE      (   '1' ) //ライントレース
+#define PETTERN_RUNNING (   '2' ) //定量走行
+#define LOOKUP_GATE     (   '3' ) //ルックアップゲート
+#define STEP            (   '4' ) //階段
+
+
 
 typedef struct {
-  SCHR event_condition      = LINE_TRACE;         //達成条件
-  int32_t flow_transition   = TRACE;              //次の遷移番号
-  UCHR action_number        = LINE_TRACE;         //実行動作番号
+  SCHR event_condition      = NO_CLEAR;   //達成条件
+  int32_t flow_transition   = TRACE;      //次の遷移番号
+  UCHR action_number        = LINE_TRACE; //実行動作番号
 }STATE_FLOW_INDEX;
 
 class ScenarioConductor_ohs {
@@ -52,6 +57,12 @@ public:
     int32_t     Milage;
     int32_t     BodyAngle;
     int32_t     TailAngle;
+
+    //目標値を入れる変数
+    SINT TargetMilage1   = 0;/* 初期値(スタートからゴールまでの距離) */
+    SINT TargetMilage2   = 0;/* 初期値(ゴールから難所通過後のグレーまでの距離) */
+    SINT TargetTailAngle = 0;/* 初期値(ルックアップゲートを通過できる角度) */
+    SINT TargetMilage3   = 0;/* 初期値(難所通過後のグレーからガレージ内停止位置までの距離) */
 
 private:
   //メンバ
