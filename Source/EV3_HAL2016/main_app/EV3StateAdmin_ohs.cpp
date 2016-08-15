0f641717517f6ce8b58f0532280aa7d8db449e0e
N-Ohtsuka <ohtsuka.nobuaki1@gmail.com>
2016-08-15 15:45:01 +0900

    ScenarioConductor作成中

---
+++ Source/EV3_HAL2016/main_app/EV3StateAdmin_ohs.cpp
@@ -0,0 +1,78 @@
+#include "ev3api.h"
+#include "hal_ev3_std.h"
+#include "BodyStateAdmin_ohs.h"
+
+/**
+ * コンストラクタ
+ */
+BodyStateAdmin_ohs::BodyStateAdmin_ohs(RayReflectAdmin_ohs* ray_reflect_admin, GyroAdmin_ohs* gyro_admin, RunningAdmin_ohs* running_admin, TailAdmin_ohs* tail_admin )
+{
+        /* 現本体状態構造体初期化 */
+        memset( mNow, 0, sizeof( BODY_STATE ) ;
+        /* 旧本体状態構造体初期化 */
+        memset( mOld, 0, sizeof( BODY_STATE ) );
+
+        mRayReflectAdmin = ray_reflect_admin;
+        mGyroAdmin       = gyro_admin;
+        mRunningAdmin    = running_admin;
+        mTailAdmin       = tail_admin;
+}
+
+/**
+ * デストラクタ
+ */
+BodyStateAdmin_ohs::~BodyStateAdmin_ohs( ) {
+}
+
+/**
+ * 本体状態更新
+ */
+void BodyStateAdmin_ohs::setBodyStateUpdate() {
+    mOld.ColorSensorState = mNow.ColorSensorState;
+    mOld.BalanceState = mNow.BalanceState;
+    mOld.Mileage = mNow.Mileage;
+    mOld.BodyAngle = mNow.BodyAngle;
+    mOld.TailAngle = mNow.TailAngle;
+
+
+   mNow.ColorSensorState = mRayReflectAdmin -> getState();
+   mNow.BalanceState = mGyroAdmin -> getState();
+   mNow.Milage = mRunningAdmin -> getMileage();
+   mNow.BodyAngle = mRunningAdmin -> getAngle();
+   mNow.TailAngle = mTailAdmin -> getTailDegree();
+}
+
+/**
+ * カラーセンサ状態取得
+ */
+SENC_CLR BodyStateAdmin_ohs::getColorSensorState() {
+  return mNow.ColorSensorState;
+}
+
+/**
+ * バランス状態取得
+ */
+GYRO_STATE BodyStateAdmin_ohs::getBalanceState() {
+  return mNow.BalanceState;
+}
+
+/**
+ * 走行距離取得
+ */
+int32_t BodyStateAdmin_ohs::getMileage() {
+  return mNow.Milage;
+}
+
+/**
+ * 本体角度取得
+ */
+int32_t BodyStateAdmin_ohs::getBodyAngle() {
+  return mNow.BodyAngle;
+}
+
+/**
+ * 尾角度取得
+ */
+int32_t BodyStateAdmin_ohs::getTailAngle() {
+  return mNow.TailAngle;
+}
--- Source/EV3_HAL2016/main_app/ScenarioConductor_ohs.h
+++ Source/EV3_HAL2016/main_app/ScenarioConductor_ohs.h
@@ -1,76 +1,63 @@
 /* by ohs50465 T.Ueno */
 
-#ifndef MAINAPP_CONTESTSCENARIOCONDUCTOR_OHS_H_
-#define MAINAPP_CONTESTSCENARIOCONDUCTOR_OHS_H_
+#ifndef MAINAPP_SCENARIOCONDUCTOR_OHS_H_
+#define MAINAPP_SCENARIOCONDUCTOR_OHS_H_
 
 #include "hal_ev3_std.h"
 #include "LineTracer_ohs.h"
 #include "BodyStateAdmin_ohs.h"
 
-/* 達成条件 */
-#define NO_CLEAR        (    0  ) //ゴールしていない
-#define TRACE_CLEAR     (    1  ) //ゴールした
-#define HURDLE1_CLEAR   (    2  ) //難所クリア
-#define HURDLE2_CLEAR   (    3  ) //ガレージインクリア
+#define SCENARIO_MAX_NUM_R  (   50 )
+#define SCENARIO_MAX_NUM_L  (   50 )
+#define SCENARIO_MAX_NUM    (  100 )
 
-/* 次の遷移番号 */
-#define TRACE           (    0  ) //トレース
-#define HURDLE1         (    1  ) //難所
-#define HURDLE2         (    2  ) //ガレージイン
+//達成条件:enum化するとEV3RT_2016_FORMと連携取り辛いので
+#define CLS_BLK    (     0 )
+#define CLS_GRY    (     1 )
+#define CLS_WHT    (     2 )
+#define RUN_MLG    (     3 )
+#define EV3_DEG    (     4 )
+#define TIL_DEG    (     5 )
+#define GYRO_ST    (     6 )
+#define EX_SLIP    (     7 )
+#define EX_END     (     8 )
+#define EVENT_NUM  (     9 )
 
-/* 実行動作番号 */
-#define LINE_TRACE      (   '1' ) //ライントレース
-#define PETTERN_RUNNING (   '2' ) //定量走行
-#define LOOKUP_GATE     (   '3' ) //ルックアップゲート
-#define STEP            (   '4' ) //階段
-
-
-
-typedef struct {
-  SCHR event_condition      = NO_CLEAR;   //達成条件
-  int32_t flow_transition   = TRACE;      //次の遷移番号
-  UCHR action_number        = LINE_TRACE; //実行動作番号
-}STATE_FLOW_INDEX;
+typedef struct _SCENARIO_INDEX {
+    UCHR move_event;   //達成条件
+    SLNG event_value;  //達成条件に付随する値
+    UCHR next_scene;   //次の遷移番号
+    UCHR pattern_id;   //実行動作番号
+}SCENE_INDEX;
 
 class ScenarioConductor_ohs {
 public:
-    //生成
-    ScenarioConductor_ohs();
+//生成
+    ScenarioConductor_ohs( BodyStateAdmin_ohs* body_state_admin, LineTracer_ohs* line_tracer, PatternSequencer_ohs* pattern_sequencer );
     //デストラクタ
-        ~ScenarioConductor_ohs();
+    ~ScenarioConductor_ohs();
 
     BOOL execScenario();                      //シナリオ実行
     void quitCommand();                       //指揮終了
-    SCHR setScenario( RUNNING_ROUTE route );  //シナリオセット
+    SCHR setScenario( UCHR ucScenNo );        //シナリオセット
     void setScenarioUpDate();                 //シナリオ更新
 
-    //メンバ
-    SENC_CLR    mColorSensorState;
-    GYRO_STATE  mBalanceState;
-    int32_t     mMilage;
-    int32_t     mBodyAngle;
-    int32_t     mTailAngle;
-
-    //記録用
-    SENC_CLR    ColorSensorState;
-    GYRO_STATE  BalanceState;
-    int32_t     Milage;
-    int32_t     BodyAngle;
-    int32_t     TailAngle;
-
-    //目標値を入れる変数
-    SINT TargetMilage1   = 0;/* 初期値(スタートからゴールまでの距離) */
-    SINT TargetMilage2   = 0;/* 初期値(ゴールから難所通過後のグレーまでの距離) */
-    SINT TargetTailAngle = 0;/* 初期値(ルックアップゲートを通過できる角度) */
-    SINT TargetMilage3   = 0;/* 初期値(難所通過後のグレーからガレージ内停止位置までの距離) */
-
 private:
-  //メンバ
-  LineTracer_ohs*     mLineTracer;
-  BodyStateAdmin_ohs* mBodyStateAdmin;
-  STATE_FLOW_INDEX    mIndex;
-  RUNNING_ROUTE       mRoute;
+    //メンバ
+    LineTracer_ohs*       mLineTracer;
+    BodyStateAdmin_ohs*   mBodyStateAdmin;
+    PatternSequencer_ohs* mPatternSequencer
 
-};
+    int         mIndex;
+    SCENE_INDEX mScenario[SCENARIO_MAX_NUM];
+    void ( *mCheckMethod[EVENT_NUM] )( void );
+
+    void checkGyro();
+    void checkRayRef();
+    void checkMileage();
+    void checkAngle();
+    void checkTailDeg();
+    void checkQuit();
+    void checkSlip();
 
 #endif  // MAINAPP_CONTESTSCENARIOCONDUCTOR_OHS_H_
